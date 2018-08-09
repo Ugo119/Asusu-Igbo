c6b441cf84fc9aed1b6814dbee64f084b39ec027
ugo119 <onuoha.ugochukwu@yahoo.com>
2018-08-09 17:47:49 +0100

    Before adding visual enhancement.

--- app/src/main/java/com/example/android/AsusuIgbo/FamilyMembers.java
+++ app/src/main/java/com/example/android/AsusuIgbo/FamilyMembers.java
@@ -1,5 +1,7 @@
 package com.example.android.AsusuIgbo;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,12 +13,32 @@ import java.util.ArrayList;
 
 public class FamilyMembers extends AppCompatActivity {
     private MediaPlayer mMediaPlayer;
-
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            releaseMediaPlayer();
+        }
+    };
+    private AudioManager mAudioManager;
+    AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener =
+            new AudioManager.OnAudioFocusChangeListener() {
+                public void onAudioFocusChange(int focusChange) {
+                    if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
+                            focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                        mMediaPlayer.pause();
+                        mMediaPlayer.seekTo(0);
+                    } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                        mMediaPlayer.start();
+                    } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                        releaseMediaPlayer();
+                    }
+                }
+            };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
-
+        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
         //Create an ArrayList to hold list of words with variable length
         final ArrayList<Word> eFamWords = new ArrayList<Word>();
         eFamWords.add(new Word("Father", "Nna", R.drawable.family_father, R.raw.nna));
@@ -36,10 +58,46 @@ public class FamilyMembers extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Word word = eFamWords.get(position);
-                mMediaPlayer = MediaPlayer.create(FamilyMembers.this,word.getAudioResourceId());
-                mMediaPlayer.start();
+                releaseMediaPlayer();
+                //Request audio focus for playback
+                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener,
+                        //Use the music stream.
+                        AudioManager.STREAM_MUSIC,
+                        //Request permanent focus.
+                        AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
 
+                    mMediaPlayer = MediaPlayer.create(FamilyMembers.this, word.getAudioResourceId());
+                    mMediaPlayer.start();
+                    mMediaPlayer.setOnCompletionListener(mCompletionListener);
+                }
             }
         });
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //when activity stops, release media player resource
+        releaseMediaPlayer();
+    }
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mMediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mMediaPlayer.release();
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mMediaPlayer = null;
+            //Abandon audio focus when playback complete
+            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
+
+
+        }
     }
 }
